e3a505cd7818de58f72bb27bb187671a498f2867
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2020-03-23 13:10:43 +0800

    React Boilerplate (Webpack)

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>React is now working with wepack!</div>
+		);
+	}
+}
+
+export default App;
